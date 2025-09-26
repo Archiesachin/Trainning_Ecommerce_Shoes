2eeb3cb1ea2b88363b8c15649558354742846088
D R <pudr@deloitte.com>
2025-09-26 17:46:48 +0530

    checkout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Homepage from './sections/Homepage';
 import Header from './components/Header';
 import Footer from './components/footer';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
 import Men from './sections/Men';
 import Women from './sections/Women';
 import New from './sections/New';
@@ -12,39 +13,30 @@ import CartDrawer from './components/CartDrawer';
 import { CartProvider, useCart } from './components/CartContext.jsx';
 import Chat from './components/chat';
 import Login from './sections/Login';
-
+import Checkout from './sections/checkout';
 
 
 function App() {
-  
+  const location = useLocation();
+  const isCheckout = location.pathname === '/checkout';
   return (
-     <CartProvider>
-    <Router>
-
-      <Header />
-      <Navbar />
-      <CartDrawer />
-      <Chat />
-
-      
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-
-        <Route path="/Men" element={<Men />} />
-
-        <Route path="/Women" element={<Women />}/>
-
-        <Route path="/signup" element={ <SignUp />} />
-
-       <Route path="/login" element={ <Login />} />
-
-        <Route path="/New" element={<New />} />
-          
-      
-
-      </Routes>
-      <Footer/>
-    </Router>
+    <CartProvider>
+      <>
+        {!isCheckout && <Header />}
+        {!isCheckout && <Navbar />}
+        <CartDrawer />
+        <Chat />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Men" element={<Men />} />
+          <Route path="/Women" element={<Women />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/New" element={<New />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+        {!isCheckout && <Footer />}
+      </>
     </CartProvider>
   );
 }
