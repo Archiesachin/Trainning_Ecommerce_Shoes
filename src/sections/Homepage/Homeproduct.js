a9136859bb6d8be9e3a4585acd290af667ac87e9
Archiesachin <archieshah8767@gmail.com>
2025-10-01 14:35:25 +0530

    homepage and homeproducts added

--- src/sections/Homepage/Homeproduct.js
+++ src/sections/Homepage/Homeproduct.js
@@ -4,6 +4,16 @@ const Homeproduct = [
     Title: "Kelly Green",
     img: "/images/Products/A11700_25Q3_Wool-Cruiser-Kelly-Green-Natural-White-Sole_PDP_LEFT__1_1.png",
     colors: '#406e42',
+    product_name: 'Womens Wool Cruiser Kelly Green',
+    images: [
+      "/images/Products/A11700_25Q3_Wool-Cruiser-Kelly-Green-Natural-White-Sole_PDP_LEFT__1_1.png",
+      "https://www.allbirds.com/cdn/shop/files/A11700_25Q3_Wool-Cruiser-Kelly-Green-Natural-White-Sole_PDP_BACK.png?v=1756336356&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11700_25Q3_Wool-Cruiser-Kelly-Green-Natural-White-Sole_PDP_TD.png?v=1756336356&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11700_25Q3_Wool-Cruiser-Kelly-Green-Natural-White-Sole_PDP_PAIR_3Q.png?v=1756336356&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11700_25Q3_Wool-Cruiser-Kelly-Green-Natural-White-Sole_PDP_SOLE.png?v=1756336356&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 100.00,
     
   },
   {
@@ -11,13 +21,22 @@ const Homeproduct = [
     Title: "Sulphur",
     img: "/images/Products/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_LEFT_6c048dbc-34ee-40b7-9e3b-4ff1b89f45cb_1.png",
     colors: '#aa7b2e',
+    product_name: 'Mens Wool Cruiser Sulphur',
+    images: [
+      "https://www.allbirds.com/cdn/shop/files/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_LEFT_6c048dbc-34ee-40b7-9e3b-4ff1b89f45cb.png?v=1756338371&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_BACK_855dd927-51ae-48cb-ac72-46e619947889.png?v=1756338371&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_TD_2ef5ea8c-0113-4b79-8ca9-20be5e5a89be.png?v=1756338374&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_PAIR_3Q_245266dc-9e22-4670-99cf-3b704636df94.png?v=1756338372&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 100.00,
   },
   {
     id: 3,
     Title: "Poppy Red",
     img: "/images/Products/A11832_25Q3_Wool-Cruiser-Poppy-Red-Natural-White-Sole_PDP_LEFT__1.png",
     colors: 'rgb(190, 50, 65)',
-    product_name: 'Womens Wool Cruiser',
+    product_name: 'Womens Wool Cruiser Poppy Red',
     images: [
       "/images/Products/A11832_25Q3_Wool-Cruiser-Poppy-Red-Natural-White-Sole_PDP_TD.png",
       "/images/Products/A11832_25Q3_Wool-Cruiser-Poppy-Red-Natural-White-Sole_PDP_BACK.png",
@@ -33,18 +52,45 @@ const Homeproduct = [
     Title: "Light Grey",
     img: "/images/Products/A11702_25Q3_Wool-Cruiser-Light-Grey-Natural-White-Sole_PDP_LEFT__1_1.png",
     colors: 'rgb(169, 168, 165)',
+    product_name: 'Womens Wool Cruiser Light Grey',
+    images: [
+      "https://www.allbirds.com/cdn/shop/files/A11702_25Q3_Wool-Cruiser-Light-Grey-Natural-White-Sole_PDP_LEFT__1.png?v=1756336876&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11702_25Q3_Wool-Cruiser-Light-Grey-Natural-White-Sole_PDP_BACK.png?v=1756336876&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11702_25Q3_Wool-Cruiser-Light-Grey-Natural-White-Sole_PDP_TD.png?v=1756336877&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11702_25Q3_Wool-Cruiser-Light-Grey-Natural-White-Sole_PDP_PAIR_3Q.png?v=1756336877&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 100.00,
   },
   {
     id: 5,
     Title: "NZ Light Blue",
     img: "/images/Products/A11837_25Q3_Wool-Cruiser-NZ-Light-Blue-Natural-White-Sole_PDP_LEFT__1.png",
     colors: 'rgb(118, 139, 173)',
+    product_name: 'Womens Wool Cruiser NZ Light Blue',
+    images: [
+      "https://www.allbirds.com/cdn/shop/files/A11837_25Q3_Wool-Cruiser-NZ-Light-Blue-Natural-White-Sole_PDP_LEFT__1.png?v=1756337773&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11837_25Q3_Wool-Cruiser-NZ-Light-Blue-Natural-White-Sole_PDP_BACK.png?v=1756337773&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11837_25Q3_Wool-Cruiser-NZ-Light-Blue-Natural-White-Sole_PDP_TD.png?v=1756337774&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11837_25Q3_Wool-Cruiser-NZ-Light-Blue-Natural-White-Sole_PDP_PAIR_3Q.png?v=1756337773&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 100.00,
   },
   {
     id: 6,
     Title: "Light Tan",
     img: "/images/Products/A11703_25Q3_Wool-Cruiser-Light-Tan-Natural-White-Sole_PDP_LEFT__1.png",
     colors: 'rgb(184, 164, 140)',
+    product_name: 'Womens Wool Cruiser Light Tan',
+    images: [
+      "https://www.allbirds.com/cdn/shop/files/A11703_25Q3_Wool-Cruiser-Light-Tan-Natural-White-Sole_PDP_LEFT__1.png?v=1756337203&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11703_25Q3_Wool-Cruiser-Light-Tan-Natural-White-Sole_PDP_BACK.png?v=1756337202&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11703_25Q3_Wool-Cruiser-Light-Tan-Natural-White-Sole_PDP_TD.png?v=1756337202&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11703_25Q3_Wool-Cruiser-Light-Tan-Natural-White-Sole_PDP_PAIR_3Q.png?v=1756337203&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 100.00,
   },
 
   
