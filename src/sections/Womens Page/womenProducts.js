b7a1845485fcaacacf9bfdc406a8c4339ae7bd22
K <maithk@deloitte.com>
2025-10-01 15:01:35 +0530

    women

--- src/sections/Womens Page/womenProducts.js
+++ src/sections/Womens Page/womenProducts.js
@@ -8,17 +8,26 @@ export const womenProducts = [
     price: 120,
     variants: [
       {
+        colorName: "Navy",
+        colorHex: "#a88256",
+        image: "public/images/Products/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_LEFT.png",
+      },
+      {
         colorName: "Dark Tan",
         colorHex: "#cd3f3fff",
         image: "public/images/Products/A11888_25Q3_Wool-Cruiser-Terracotta-Natural-White-Sole_PDP_LEFT__1_fcef493a-0b6b-4785-9d93-ec4931435ef4_1.png",
-      },
-      {
-        colorName: "Navy",
-        colorHex: "#A88256",
-        image: "public/images/Products/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_LEFT.png",
       }
     ],
     sizes: [],
+    images: [
+      "https://www.allbirds.com/cdn/shop/files/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_LEFT_6c048dbc-34ee-40b7-9e3b-4ff1b89f45cb.png?v=1756338371&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_BACK_855dd927-51ae-48cb-ac72-46e619947889.png?v=1756338371&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_TD_2ef5ea8c-0113-4b79-8ca9-20be5e5a89be.png?v=1756338374&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_SOLE_0ae4ea5d-3365-47bc-937d-bf4758608e65.png?v=1756338371&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11882_25Q3_Wool-Cruiser-Sulphur-Natural-White-Sole_PDP_PAIR_3Q_245266dc-9e22-4670-99cf-3b704636df94.png?v=1756338372&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 120.00
   },
   {
     id: 2,
@@ -42,6 +51,15 @@ export const womenProducts = [
     
     
     sizes: [],
+    images: [
+      "https://www.allbirds.com/cdn/shop/files/A11631_25Q3_Wool-Cruiser-Dark-Camel-Natural-White-Sole_PDP_LEFT__1_bec494cd-90bc-4d1a-b180-cd8a56464781.png?v=1756335125&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11631_25Q3_Wool-Cruiser-Dark-Camel-Natural-White-Sole_PDP_LEFT__1_bec494cd-90bc-4d1a-b180-cd8a56464781.png?v=1756335125&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11631_25Q3_Wool-Cruiser-Dark-Camel-Natural-White-Sole_PDP_SOLE_9759537c-aca8-4687-94dc-937cd67b5bae.png?v=1756335126&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11631_25Q3_Wool-Cruiser-Dark-Camel-Natural-White-Sole_PDP_TD_6351ca85-0812-4e15-aaf5-ab5bc5d484d0.png?v=1756335126&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11631_25Q3_Wool-Cruiser-Dark-Camel-Natural-White-Sole_PDP_PAIR_3Q_ea4d02bc-bbe8-4908-b87d-cee431794bfe.png?v=1756335126&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 100.00
   },
   {
     id: 3,
@@ -62,6 +80,16 @@ export const womenProducts = [
       }
     ],
     sizes: [],
+    images: [
+      "https://www.allbirds.com/cdn/shop/files/A11768_25Q4_Strider-Explore-Natural-Black-Dark-Grey-Sole_PDP_LEFT_1112741e-9d4e-4850-b6d4-913146a82779.png?v=1758235923&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11768_25Q4_Strider-Explore-Natural-Black-Dark-Grey-Sole_PDP_BACK_afe2e3ea-247d-4b75-84e8-ceebc85f6e15.png?v=1758235923&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11768_25Q4_Strider-Explore-Natural-Black-Dark-Grey-Sole_PDP_TD_fb71768a-20ec-4509-9e67-9518ac7bb267.png?v=1758235924&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11768_25Q4_Strider-Explore-Natural-Black-Dark-Grey-Sole_PDP_SOLE_66f1bd19-6fa1-4b98-bf2e-c159be5708d2.png?v=1758235922&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11768_25Q4_Strider-Explore-Natural-Black-Dark-Grey-Sole_PDP_PAIR_3Q_393b0080-5376-47ec-ad36-6aa471c74a16.png?v=1758235922&width=1024"
+    ],
+    description: "Our most cushioned shoe, built for all-day comfort and long walks.",
+    price: 135.00
+
   },
   {
     id: 4,
@@ -85,6 +113,15 @@ export const womenProducts = [
     ],
     extraColors: 0,
     sizes: [],
+    images: [
+      "https://www.allbirds.com/cdn/shop/files/A11636_25Q3_Wool-Cruiser-Slip-On-Dark-Grey-Light-Grey-Sole_PDP_LEFT__1_056967a9-8421-46db-a3ed-cae6cdae557a.png?v=1754513164&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11636_25Q3_Wool-Cruiser-Slip-On-Dark-Grey-Light-Grey-Sole_PDP_BACK_43adc80e-c022-45fb-8daf-21ff8ceb6818.png?v=1754513164&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11636_25Q3_Wool-Cruiser-Slip-On-Dark-Grey-Light-Grey-Sole_PDP_TD_b0dd3bfd-707c-483a-ae3a-8e3429510718.png?v=1754513165&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11636_25Q3_Wool-Cruiser-Slip-On-Dark-Grey-Light-Grey-Sole_PDP_SOLE_551959af-1b9a-4585-8552-b198ed6854c6.png?v=1754513164&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11636_25Q3_Wool-Cruiser-Slip-On-Dark-Grey-Light-Grey-Sole_PDP_PAIR_3Q_61d42c50-2974-4214-af27-631cba1c247f.png?v=1754513164&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 160.00,
   },
   {
     id: 5,
@@ -107,6 +144,15 @@ export const womenProducts = [
     ],
     extraColors: 0,
     sizes: [],
+    images: [
+      "https://www.allbirds.com/cdn/shop/files/A12011_25Q3_Wool_Runner_NZ_Dark_Navy_Blizzard_Sole_PDP_LEFT_7480f37d-ec84-452d-9ad2-41186ae54551.png?v=1753728659&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A12011_25Q3_Wool_Runner_NZ_Dark_Navy_Blizzard_Sole_PDP_BACK_62581118-331d-4f23-8b61-12c77599f9a0.png?v=1753728658&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A12011_25Q3_Wool_Runner_NZ_Dark_Navy_Blizzard_Sole_PDP_TD_3fd0ef1b-7cbf-4f68-9543-85d7ab11b13b.png?v=1753728659&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A12011_25Q3_Wool_Runner_NZ_Dark_Navy_Blizzard_Sole_PDP_SOLE_7aad9c2e-ec1e-4336-a21e-e660e28bd417.png?v=1753728658&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A12011_25Q3_Wool_Runner_NZ_Dark_Navy_Blizzard_Sole_PDP_PAIR_3Q_b76f8a8a-d913-417e-9d94-36c21b354243.png?v=1753728659&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 90.00,
   },
   {
     id: 6,
@@ -128,6 +174,15 @@ export const womenProducts = [
     ],
     extraColors: 0,
     sizes: [],
+    images: [
+      "https://www.allbirds.com/cdn/shop/files/A11959_25Q3_Tree_Runner_NZ_Medium_Grey_Blizzard_Sole_PDP_LEFT-2000x2000_f6ff6164-62df-42dc-89a8-fabcb3b5d438.png?v=1751899014&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11959_25Q3_Tree_Runner_NZ_Medium_Grey_Blizzard_Sole_PDP_BACK-2000x2000_a95c1083-3636-43e5-a742-78c9a26a2936.png?v=1751899014&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11959_25Q3_Tree_Runner_NZ_Medium_Grey_Blizzard_Sole_PDP_TD-2000x2000_57af9e6f-e170-4bb5-911e-232a0bdc880f.png?v=1751899014&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11959_25Q3_Tree_Runner_NZ_Medium_Grey_Blizzard_Sole_PDP_SOLE-2000x2000_6de769a9-6ea9-46ab-8a72-fe2a595a695f.png?v=1751899014&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11959_25Q3_Tree_Runner_NZ_Medium_Grey_Blizzard_Sole_PDP_PAIR_3Q-2000x2000_b2a2dd7a-00a7-4687-8c61-aa6e44f8041c.png?v=1751899014&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 121.00
   },
   {
     id: 7,
@@ -149,6 +204,15 @@ export const womenProducts = [
     ],
     extraColors: 0,
     sizes: [],
+    images: [
+      "https://www.allbirds.com/cdn/shop/files/AB00FVM090_SHOE_PROFILE_GLOBAL_MENS_TREE_RUNNER_HAZY_INDIGO_BLIZZARD_6aa55bbf-dbb2-4830-9c13-ba0927aa024a.png?v=1751165366&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/AB00FVM090_SHOE_HEEL_GLOBAL_MENS_TREE_RUNNER_HAZY_INDIGO_BLIZZARD_5f245d61-7fb6-4c5e-881e-f79f12150810.png?v=1751165365&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/AB00FVM090_SHOE_OVERHEAD_GLOBAL_MENS_TREE_RUNNER_HAZY_INDIGO_BLIZZARD_13c22dd8-b07e-40ed-8961-16f7655ad8fc.png?v=1751165365&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/AB00FVM090_SHOE_BOTTOM_GLOBAL_MENS_TREE_RUNNER_HAZY_INDIGO_BLIZZARD_1f7a6b76-9508-46e9-85a9-3a81eeb8d4a2.png?v=1751165365&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/AB00FVM090_SHOE_PAIR_GLOBAL_MENS_TREE_RUNNER_HAZY_INDIGO_BLIZZARD_7a528d83-9f9d-4eba-b050-79895a2663dd.png?v=1751165365&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 139.00 
   },
   {
     id: 8,
@@ -168,7 +232,18 @@ export const womenProducts = [
         colorHex: "#b1b8afff",
         image: "public/images/Products/A11702_25Q3_Wool-Cruiser-Light-Grey-Natural-White-Sole_PDP_LEFT__1_1.png",
       },
-    ]},
+    ],
+    images: [  
+      "https://www.allbirds.com/cdn/shop/files/A11700_25Q3_Wool-Cruiser-Kelly-Green-Natural-White-Sole_PDP_LEFT__1.png?v=1756336356&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11700_25Q3_Wool-Cruiser-Kelly-Green-Natural-White-Sole_PDP_BACK.png?v=1756336356&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11700_25Q3_Wool-Cruiser-Kelly-Green-Natural-White-Sole_PDP_TD.png?v=1756336356&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11700_25Q3_Wool-Cruiser-Kelly-Green-Natural-White-Sole_PDP_SOLE.png?v=1756336356&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11700_25Q3_Wool-Cruiser-Kelly-Green-Natural-White-Sole_PDP_PAIR_3Q.png?v=1756336356&width=1024"      
+       ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 140.00
+
+  },
   {
     id: 9,
     label: "New",
@@ -188,6 +263,15 @@ export const womenProducts = [
         image: "public/images/mens/A11573_25Q3_Tree_Breezer_Bow_Bark_Brown_PDP_LEFT-2000x2000.png",
       }
     ],
+    images: [ 
+      "https://www.allbirds.com/cdn/shop/files/A11738_25Q3_Tree_Breezer_Bow_Natural_Black_PDP_LEFT-2000x2000.png?v=1751993280&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11738_25Q3_Tree_Breezer_Bow_Natural_Black_PDP_BACK-2000x2000.png?v=1751993280&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11738_25Q3_Tree_Breezer_Bow_Natural_Black_PDP_TD-2000x2000.png?v=1751993280&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11738_25Q3_Tree_Breezer_Bow_Natural_Black_PDP_SOLE-2000x2000.png?v=1751993280&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A11573_25Q3_Tree_Breezer_Bow_Bark_Brown_PDP_PAIR_3Q-2000x2000.png?v=1756335125&width=1024"      
+       ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 140.00       
   }
  ,
   {
@@ -210,6 +294,15 @@ export const womenProducts = [
         
       }
     ],
+    images: [ 
+      "https://www.allbirds.com/cdn/shop/files/A12113_25Q3_Anytime_Soft_Merino_Tee_Deep_Navy_PDP_F-2000x2000.png?v=1755543937&width=1024",      
+      "https://www.allbirds.com/cdn/shop/files/A12113_25Q3_Anytime_Soft_Merino_Tee_Deep_Navy_PDP_D-2000x2000.png?v=1755543937&width=1024",  
+      "https://www.allbirds.com/cdn/shop/files/A12113_25Q3_Anytime_Soft_Merino_Tee_Deep_Navy_PDP_F2-2000x2000.png?v=1755543937&width=1024",
+      "https://www.allbirds.com/cdn/shop/files/A12113_25Q3_Anytime_Soft_Merino_Tee_Deep_Navy_PDP_D2-2000x2000.png?v=1755543937&width=1024"
+    
+       ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 140.00
   },
   {
     id: 11,
@@ -226,6 +319,13 @@ export const womenProducts = [
       },
       
     ],
+    images: [ 
+      "https://www.allbirds.com/cdn/shop/files/A11541_24Q4_Recycled_Tote_Bag_Rugged_Beige_PDP_Front_25059-1300x1300.png?v=1733786045&width=1024",      
+      "https://www.allbirds.com/cdn/shop/files/A11541_24Q4_Recycled_Tote_Bag_Rugged_Beige_PDP_Back_25063-1300x1300.png?v=1733786045&width=1024",  
+      "https://www.allbirds.com/cdn/shop/files/A11541_24Q4_Recycled_Tote_Bag_Rugged_Beige_PDP_Angle_25288-1300x1300.png?v=1733786045&width=1024"
+    ],
+    description: "Lightweight comfort and classic style crafted with recycled Italian felt wool.",
+    price: 140.00
   },
 
  
